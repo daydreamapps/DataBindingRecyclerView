7ec43b2c45c8c8aeb1ddd534b4f4071b875ecae1
Samuel Patmore <daydreamapplications@gmail.com>
2020-11-02 20:48:41 +0000

    Update DiffUtilAddRemoveActivity to Use MultiTypeAdapter

--- app/src/main/java/com/daydreamapplications/sample/DiffUtilAddRemoveActivity.kt
+++ app/src/main/java/com/daydreamapplications/sample/DiffUtilAddRemoveActivity.kt
@@ -1,21 +1,24 @@
 package com.daydreamapplications.sample
 
 import android.os.Bundle
-import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import com.daydreamapplications.bindingrecycler.BindingRecyclerView
 import com.daydreamapplications.sample.databinding.ActivityAddRemoveBinding
 
 class DiffUtilAddRemoveActivity : AppCompatActivity() {
 
-    private val adapter = Adapter()
+    private val adapter = MultiTypeAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         supportActionBar?.title = getString(R.string.diff_util_add_remove)
 
-        adapter.viewModels = listOf("0", "1", "2", "3")
+        adapter.viewModels = listOf(
+            MultiTypeAdapter.Models.Text("0"),
+            MultiTypeAdapter.Models.Text("1"),
+            MultiTypeAdapter.Models.Text("2"),
+            MultiTypeAdapter.Models.Text("3")
+        )
 
         ActivityAddRemoveBinding.inflate(layoutInflater).apply {
             setContentView(root)
@@ -24,8 +27,8 @@ class DiffUtilAddRemoveActivity : AppCompatActivity() {
             // Add new item to end of list
             buttonAddItem.setOnClickListener {
                 val currentItems = adapter.viewModels
-                val newItem = currentItems.size.toString()
-                adapter.viewModels = currentItems + newItem
+                val newItemText = currentItems.size.toString()
+                adapter.viewModels = currentItems + MultiTypeAdapter.Models.Text(newItemText)
             }
 
             // Remove last item in list
@@ -37,11 +40,5 @@ class DiffUtilAddRemoveActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    class Adapter : BindingRecyclerView.Adapter<String>() {
-
-        @LayoutRes
-        override fun getItemLayoutRes(position: Int): Int = R.layout.item_string
     }
 }
